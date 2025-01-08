8ac90675e40c65d4f0cfef8beaf828b467b57ace
hasanulgic <hasanulgic@gmail.com>
2025-01-08 17:59:15 +0600

    mostly done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Image from "./component/Image"
+import Table from "./component/Table"
+
+function App() {
+
+  return (
+    <div className='mx-auto max-w-4xl mt-10'>
+      {/* <Table/> */}
+      <Image/>
+    </div>
+  )
+}
+
+export default App
